12d57b63cf76e40667849b5935a572ac00b88993
Asket <Asket.pt@gmail.com>
2024-12-05 23:20:02 +0200

    3

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*1*/
+function f() {
+    alert('Ви натиснули на кнопку #3');
+}
+/*2*/
+btn1.onclick = function () {
+    alert('Ви натиснули на кнопку #1');
+}
+/*3*/
+var btn2 = document.getElementById("btn2")
+btn2.onclick = function () {
+    alert('Ви натиснули на кнопку #2');
+}
