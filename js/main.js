3dd8878713b0be1cd3d7f5fbe71c935ae41efad0
Asket <Asket.pt@gmail.com>
2024-12-18 00:54:07 +0200

    0

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ function f() {
 /*2*/
 btn1.onclick = function () {
     alert('Ви натиснули на кнопку #1');
+    window.dataLayer.push({
+        event: 'btn1_click',  // Название события
+        event_category: 'engagement',  // Категория события
+        event_label: 'btn1_click_clicked',  // Метка события
+        value: 1  // Дополнительное значение
+    });
+
+
 }
 /*3*/
 var btn2 = document.getElementById("btn2")
